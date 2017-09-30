ad4ff992616e4544a9aae492029f2a42a68a76f6
Bastian Kröger <bastian.kroeger@outlook.com>
2017-09-30 10:32:30 +0200

    Projektdateien hinzufügen.

---
+++ LogBasedStuff/ElectronicMailing.vb
@@ -0,0 +1,11 @@
+﻿Public Class ElectronicMailing
+
+    Public Property Id As String = Guid.NewGuid.ToString
+    Public Property Template As String
+    Public Property Filter As String
+
+    Public Function InsertAsync() As Task
+        Return Task.completed
+    End Function
+
+End Class
